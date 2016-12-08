e43d117be13384dde0ceef35e0a4271953b012b3
jay0lu <luzheng1992@gmail.com>
2016-12-08 17:09:02 -0500

    correct loop error
    add break to exit loop

--- longest_string_without_repeating.py
+++ longest_string_without_repeating.py
@@ -1,13 +1,13 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
 class Solution(object):
-    def lengthOfLongestSubstring(s):
+    def lengthOfLongestSubstring(self,s):
         """
         :type s: str
         :rtype: int
         """
 
-        longest_len = 1
+        longest_len = 0
         for a in range(0,len(s)):
             list = []
             list.append(s[a])
@@ -16,13 +16,19 @@ class Solution(object):
                 if not s[b] in list:
                     list.append(s[b])
                     print(list)
+                else: break
 
             if len(list) > longest_len:
                 longest_len = len(list)
 
-        print("longest length is %s" % longest_len)
+        return longest_len
 
 
 str = "abcabcbb"
 str2 = "aaaaaaa"
-Solution.lengthOfLongestSubstring(str)
+str3 = "pwwkew"
+str4 = "abdddce"
+str5 = "addccccddddddd"
+solution = Solution()
+length = solution.lengthOfLongestSubstring(str5)
+print("longest length is %s" % length)
