c194ee73a6717c19c7373d8fb0f185683f98f346
jay0lu <luzheng1992@gmail.com>
2016-12-08 13:23:57 -0500

    add two solutions

--- two_sum.py
+++ two_sum.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/two-sum/
+
 class Solution(object):
     def twoSum(nums, target):
         """
@@ -7,8 +9,33 @@ class Solution(object):
         """
         for a in range(0,len(nums)-1):
             for b in range(a,len(nums)):
-                pass
+                added = nums[a] + nums[b]
+                if added == target:
+                    print(a,b)
+
+
+    # better solution, using hash table(dir)
+    def twoSumHash(nums, target):
+        # store list to dir
+        D = {}
+        for a in range(0,len(nums)):
+            D[a] = nums[a]
+        # find the value
+        for x in range(0,len(nums)):
+            target_value = target - nums[x]
+            # print(list(D.values()))
+            if target_value in D.values():
+                # convert dict values to list, find list index
+                target_index = list(D.values()).index(target_value)
+                print(x,target_index)
+                break
+
+
 
 nums = [2, 7, 11, 15]
 target = 9
+print("brute force result:")
 Solution.twoSum(nums,target)
+print("Dict(hash table) result:")
+Solution.twoSumHash(nums,target)
+# print(nums[1])
