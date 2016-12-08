4c10898b3d929395cda4f05ec700194e13d189da
jay0lu <luzheng1992@gmail.com>
2016-12-08 11:52:54 -0500

    first try

---
+++ two_sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for a in range(0,len(nums)-1):
+            for b in range(a,len(nums)):
+                pass
+
+nums = [2, 7, 11, 15]
+target = 9
+Solution.twoSum(nums,target)
