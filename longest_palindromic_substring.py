11b5c65fcfb591c92e78262b3f7418f24ca3dbd1
jay0lu <luzheng1992@gmail.com>
2016-12-11 16:39:12 -0500

    using dynamic programming

---
+++ longest_palindromic_substring.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/longest-palindromic-substring/
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
