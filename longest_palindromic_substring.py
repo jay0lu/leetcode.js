3a6be7cef3b3ea3df188b0a73e1d454990e8a46b
jay0lu <luzheng1992@gmail.com>
2016-12-11 17:56:47 -0500

    fix the palindromic problem

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -1,9 +1,32 @@
 # https://leetcode.com/problems/longest-palindromic-substring/
 
-class Solution(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        
+def check_palindrome(str):
+    if len(str) > 1:
+        if str[0] == str[-1]:
+            if len(str[1:-1]) > 0:
+                check_palindrome(str[1:-1])
+                return True
+        else:
+            return False
+    else:
+        return True
+
+
+# class Solution(object):
+#     def longestPalindrome(self, s):
+#
+#         # dynamic programming
+#         if len(s) < 2:
+#             return s
+#         else:
+#             slide_window = 1
+#             i = 0
+#             j = 0
+#             while i+slide_window < len(s):
+#                 j = i + slide_window
+#                 for x in range (0,len(s)):
+#                     # check_palindrome(s[i:j])
+#                     pass
+
+s = "abc"
+print(check_palindrome(s))
